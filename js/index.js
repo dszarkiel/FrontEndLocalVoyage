793d02338b425a3b6381fe626036be9075c97863
dszarkiel <d.szarkiel@gmail.com>
2020-12-24 11:42:23 -0600

    false commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
-let CURRENT_USER = ""
+let currentUser = ""
+let allUsers = []
 
-let signIn = document.querySelector("#signin")
+const signIn = document.querySelector("li#sign-in")
+const welcomeScreen = document.querySelector("div#welcome-screen")
+const signInForm = document.querySelector("form.sign-in-form")
 
-
+function main(){
+    fetchAllUsers()
+    console.log(allUsers)
+}
 signIn.addEventListener("click", () => {
     showSignInDiv()
 })
-
 function showSignInDiv() {
-    let signInDiv = document.querySelector(".signin-card")
-    
+    let signInDiv = document.querySelector(".sign-in-card")
+    console.log("this works")
+    welcomeScreen.style.display = "none"
     signInDiv.hidden = false
 } 
+signInForm.addEventListener("submit", submitSignIn)
+function submitSignIn(e){
+    e.preventDefault()
+    let userEmail = e.target["email"].value
+    if allUsers.find(user ==> )
+}
+
+function fetchAllUsers(){
+    fetch("http://localhost:3000/users")
+    .then(resp => resp.json())
+    .then(usersObj => {
+        allUsers = usersObj
+        use
+    })
+}
+////////// Initialize //////////
+main()
