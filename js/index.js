ae2e422ecbd0fa2368d5d6eb53ba1324c0cf0f20
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2021-01-01 11:39:40 -0600

    pulled sam up to date

--- js/index.js
+++ js/index.js
@@ -61,7 +61,6 @@ function showSignInDiv() {
     welcomeScreen.style.display = "none"
     signInDiv.hidden = false
     signUpDiv.hidden = true
-    cardDiv.hidden = true
 } 
 
 signInForm.addEventListener("submit", submitSignIn)
@@ -74,7 +73,6 @@ function submitSignIn(e){
         signInDiv.hidden = true
         logInUL.hidden = true
         userUL.hidden = false
-        cardDiv.hidden = true
         renderDashboard(currentUser)
     } else {
         alert("Username does not exist!")
@@ -92,7 +90,6 @@ function showSignUpDiv() {
     welcomeScreen.style.display = "none"
     signUpDiv.hidden = false
     signInDiv.hidden = true
-    cardDiv.hidden = true
 } 
 
 signUPForm.addEventListener("submit", submitSignUp)
@@ -128,7 +125,7 @@ function renderMyDestinations() {
     myDestinations.forEach(dest => {
         if (dest.visited === true) {
             let visitedLi = document.createElement("li")
-            visitedLi.textContent = dest.name 
+            visitedLi.textContent = dest.name
             visitedLi.dataset.destId = dest.id
             visitedLi.addEventListener("click", renderCard)
             visitedUl.append(visitedLi)
@@ -141,121 +138,6 @@ function renderMyDestinations() {
         }
     })
 }
-
-//////////// RENDER DESTINATION CARD///////////
-function renderCard(e) {
-    const id = e.target.dataset.destId
-
-    fetch(`http://localhost:3000/destinations/${id}`)
-    .then(response => response.json())
-    .then(destination => makeNewDestCard(destination))
-}
-///////// MAKE DESTINATION CARD //////////////
-function makeNewDestCard(destination) {
-    const exitBtn = document.querySelector("#exit")
-    exitBtn.addEventListener("click", exitOut)
-    const showPage = document.querySelector(".show-page")
-    const name = document.createElement('h2')
-    name.innerText = destination.name
-    const img = document.createElement('img')
-    img.src = "AddImage"
-    const dateVisited = document.createElement('h3')
-    dateVisited.innerText = destination.date_visited
-    const address = document.createElement('p')
-    address.innerText = ` Address: ${document.address}`
-    const category = document.createElement('p')
-    category.innerText = `Category: ${destination.category}`
-    const comment = document.createElement('p')
-    comment.innerText = `Comments: ${destination.comment}`
-    const visited = document.createElement('p')
-    visited.innerText = `Visited? ${destination.visited}`
-    const cost = document.createElement('p')
-    cost.innerText = `Cost: ${destination.cost}`
-    const attendees = document.createElement('p')
-    attendees.innerText = `Attendees: ${destination.attendees}`
-    const rating = document.createElement('p')
-    rating.innerText = `${destination.rating} stars`
-    const hr1 = document.createElement('hr')
-    const hr2 = document.createElement('hr')
-    const editBtn = document.createElement('button')
-    editBtn.innerText = "Edit"
-    const deleteBtn = document.createElement('button')
-    deleteBtn.innerText = "Delete"
-    cardDiv.append(name, img, dateVisited, hr1, address, category, visited, cost, attendees, hr2, comment, rating, editBtn, deleteBtn)
-    cardDiv.hidden = false
-    cardDiv.style.display = "block"
-    showPage.hidden = true
-    userDashboard.append(cardDiv)
-    
-}
-
-////////// EXIT OUT OF SHW CARD/////////////
-function exitOut(e){
-    console.log(e.target)
-    const showPage = document.querySelector(".show-page")
-    const cardDiv = document.querySelector(".destination-card")
-    cardDiv.hidden = true
-    showPage.hidden = false
-    cardDiv.style.display = "none"
-    userDashboard.append(showPage)
-}
-
-//////////// ADD NEW DESTINATION//////////////
-newDestForm.addEventListener("submit", addNewDestination)
-function addNewDestination(e){
-    e.preventDefault()
-    console.log(e.target)
-    const destName = document.querySelector("#google-search").value
-    const destDate = document.querySelector("#date").value
-    const destAddress = document.querySelector("#address").value
-    const destCategory = document.querySelector("#category").value
-    const destComment = document.querySelector("#comment").value
-    const destVisited = document.querySelector("#visited").value
-    const destCost = document.querySelector("#cost").value
-    const destAttendees = document.querySelector("#attendees").value
-    const destRating = document.querySelector("#rating").value
-
-    let visitedUl = document.querySelector(".visited-ul")
-    let notVisitedUl = document.querySelector(".not-visited-ul")
-    if (destVisited === true) {
-        let visitedLi = document.createElement("li")
-        visitedLi.textContent = destName 
-        visitedLi.dataset.destId = dest.id
-        visitedLi.addEventListener("click", renderCard)
-        visitedUl.append(visitedLi)
-    } else {
-        let notVisitedLi = document.createElement("li")
-        notVisitedLi.textContent = destName 
-        notVisitedLi.addEventListener("click", renderCard)
-        notVisitedUl.append(notVisitedLi)
-    }
-
-    fetch("http://localhost:3000/destinations", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            name: destName,
-            date_visited: destDate,
-            address: destAddress, 
-            category: destCategory, 
-            comment: destComment,
-            visited: destVisited,
-            cost: destCost,
-            attendees: destAttendees,
-            rating: destRating,
-            user_id: currentUser.id
-        })
-    })
-    .then(response => response.json())
-    .then(destination=> renderMyDestinations(destination))
-    
-}
-
-
-
-
 
 //////////// RENDER GOOGLE MAPS ON DASHBOARD //////////// 
 function initMap() {
