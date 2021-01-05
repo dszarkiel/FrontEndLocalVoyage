4ee79889d8797e7109dcf0db1d69f97e24a1caf1
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2021-01-05 13:37:38 -0600

    sam

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ fetchAllDestinations();
 function renderDashboard(user) {
     currentUser = user
     userDashboard.style.display = "flex"
+    secondDashboardDiv.style.display = "flex"
     initMap();
     initAutocomplete();
     renderMyDestinations();
@@ -16,6 +17,8 @@ let allUsers;
 let currentUser;
 let allDestinations;
 let map;
+let mapMarkers = [];
+// Sign-in and Sign-Up variables
 const signIn = document.querySelector("li#sign-in")
 const signUp = document.querySelector("li#sign-up")
 const welcomeScreen = document.querySelector("div#welcome-screen")
@@ -25,7 +28,9 @@ const signInDiv = document.querySelector(".sign-in-card")
 const signUpDiv = document.querySelector(".sign-up-card")
 const logInUL = document.querySelector("ul.nav")
 const userUL = document.querySelector("ul.user-nav")
+// Rendering current user dashboard
 const userDashboard = document.querySelector(".dashboard")
+const secondDashboardDiv = document.querySelector(".logo-create-new")
 const newDestinationForm = document.querySelector("form.new-destination-form")
 const destinationFormDiv = document.querySelector("div.destination-form")
 const showCard = document.querySelector("div.show-card")
@@ -33,6 +38,21 @@ const createNewBtn = document.querySelector("button.create-new-destination-btn")
 const destinationList = document.querySelector("div.destinations-list")
 const googleMaps = document.querySelector(".google-maps")
 const cancelFormBtn = document.querySelector(".cancelFormBtn")
+// Account info and account update variables
+const accountNavBtn = document.querySelector("#account-view") 
+const logOutBtn = document.querySelector("#sign-out") 
+const accountInfoCard = document.querySelector(".account-info-card") 
+const editAccountCard = document.querySelector(".edit-account-card")
+const editAccountForm = document.querySelector("#edit-account-form")
+// Edit existing destination information variables 
+const editDestDiv = document.querySelector(".edit-destination-div")
+const editDestForm = document.querySelector(".edit-destination-form")
+const editDestCancelBtn = document.querySelector(".cancelDestUpdateBtn")
+// Account information to render on account show card
+let firstName = document.querySelector(".account-info-card #first_name");
+let lastName = document.querySelector(".account-info-card #last_name");
+let email = document.querySelector(".account-info-card #email");
+
 
 
 //////////// EVENT LISTENER FOR GOOGLE MAPS ///////////
@@ -40,17 +60,12 @@ googleMaps.addEventListener("click", googleMapShowCard)
 
 function googleMapShowCard(e){
     if(e.target.tagName==="H4"){
-
     let id = e.target.id
-
     fetch(`http://localhost:3000/destinations/${id}`)
     .then(response => response.json())
     .then(destination => makeNewDestCard(destination))
     }
 }   
-
-
-
 
 //////////// FETCH ALL USERS IN DB //////////// 
 function fetchAllUsers(){
@@ -65,10 +80,9 @@ function fetchAllDestinations() {
     fetch("http://localhost:3000/destinations")
     .then(resp => resp.json())
     .then(destObj => {
-        allDestinations = destObj
+        allDestinations = destObj;
     })
 } 
-
 
 //////////// SIGN IN LOGIC //////////// 
 signIn.addEventListener("click", () => {
@@ -134,7 +148,6 @@ function submitSignUp(e) {
     userUL.hidden = false
 }
 
-
 //////////// RENDER ALL EXISTING DESTINATIONS IN DESTINATIONS LIST ////////////
 function renderMyDestinations() {
     let myDestinations = allDestinations.filter(dest => dest.user_id === currentUser.id)
@@ -183,12 +196,17 @@ function initAutocomplete() {
           document.querySelector("#address").value = nearPlace.formatted_address;
           document.querySelector("#latitude").value = nearPlace.geometry.location.lat();
           document.querySelector("#longitude").value = nearPlace.geometry.location.lng();
+
       })
 }
 
 //////////// ADDS DESTINATION MARKERS TO MAP FROM DB //////////// 
 function renderMapMarker(map) {
     let myDestinations = allDestinations.filter(dest => dest.user_id === currentUser.id)
+    if (mapMarkers) {
+        mapMarkers.forEach(marker => marker.setMap(null))
+    }
+
     if (myDestinations) {
         myDestinations.forEach(dest => {
             addMarker(dest, map);
@@ -234,8 +252,8 @@ newDestinationForm.addEventListener("submit", (e) => {
 })
 
 
+////////// RENDER NEW GOOGLE MAP MARKER //////////////
 function addMarker(dest, map) {
-   
     if (dest.visited === true){
         let marker = new google.maps.Marker({
             position:{lat: dest.latitude, lng: dest.longitude},
@@ -250,7 +268,7 @@ function addMarker(dest, map) {
         let infoWindow = new google.maps.InfoWindow({
             content: objectDetails
         })
-
+        mapMarkers.push(marker)
         marker.addListener("click", () => {
             infoWindow.open(map, marker)
         })
@@ -268,19 +286,18 @@ function addMarker(dest, map) {
         let infoWindow = new google.maps.InfoWindow({
             content: objectDetails
         })
-
+        mapMarkers.push(marker)
         marker.addListener("click", () => {
             infoWindow.open(map, marker)
         })
-    }
-        
+    } 
 }
 
-//////////SAM ADDED //////////
+////////// ADD NEW DESTINATION TO LIST (VISITED / NOT VISITED) //////////
 function addNewDestination(dest) {
     let visitedUl = document.querySelector(".visited-ul")
     let notVisitedUl = document.querySelector(".not-visited-ul")
-    if (dest.visited === "Yes") {
+    if (dest.visited === true) {
         let visitedLi = document.createElement("li")
         visitedLi.textContent = dest.name 
         visitedLi.dataset.destId = dest.id
@@ -295,11 +312,16 @@ function addNewDestination(dest) {
         }
 }
 
+////////// RENDER CREATE NEW DESTINATION FORM //////////////
  createNewBtn.addEventListener("click", renderNewForm)
-
  function renderNewForm(){
     destinationList.hidden = true
     destinationFormDiv.hidden = false
+    showCard.hidden = true
+    editAccountCard.hidden = true
+    editAccountForm.hidden = true
+    accountInfoCard.hidden = true
+    editDestDiv.hidden = true
  }
 
  cancelFormBtn.addEventListener("click", () => {
@@ -318,17 +340,19 @@ function renderCard(e) {
 }
 ///////// MAKE DESTINATION CARD //////////////
 function makeNewDestCard(destination){
-
+    showCard.innerHTML = ""
     const exitBtn = document.createElement("span")
     exitBtn.innerText = "X"
     exitBtn.addEventListener("click", exitOut)
-
     const name = document.createElement('h2')
     name.innerText = destination.name
-    const img = document.createElement('img')
-    img.src = "AddImahe"
-    const dateVisited = document.createElement('h3')
-    dateVisited.innerText = destination.date_visited
+    // const img = document.createElement('img')
+    // img.src = destination.image
+    const dateVisited = document.createElement('p')
+    dateVisited.innerText = `Date: ${destination.date_visited}`
+    if (destination.date_visited === "" && destination.visited) {
+        dateVisited.innerText = "Date: Please enter the date you attended!"
+    }
     const address = document.createElement('p')
     address.innerText = ` Address: ${destination.address}`
     const category = document.createElement('p')
@@ -336,9 +360,13 @@ function makeNewDestCard(destination){
     const comment = document.createElement('p')
     comment.innerText = `Comments: ${destination.comment}`
     const visited = document.createElement('p')
-    visited.innerText = `Visited? ${destination.visited}`
+    if (destination.visited) {
+        visited.innerHTML = "Visited: &#9989;"
+    } else {
+        visited.innerHTML = "Visited: &#10060;"
+    }
     const cost = document.createElement('p')
-    cost.innerText = `Cost: ${destination.cost}`
+    cost.innerText = `Cost: $${destination.cost}`
     const attendees = document.createElement('p')
     attendees.innerText = `Attendees: ${destination.attendees}`
     const rating = document.createElement('p')
@@ -346,33 +374,198 @@ function makeNewDestCard(destination){
     const hr1 = document.createElement('hr')
     const hr2 = document.createElement('hr')
     const hr3 = document.createElement('hr')
-    const editBtn = document.createElement("button")
-    editBtn.innerText = "Edit Memory"
-    editBtn.dataset.id = destination.id
-    editBtn.addEventListener("click", editShowCard)
-    const deleteBtn = document.createElement("button")
-    deleteBtn.innerText = "Delete Memory"
-    deleteBtn.dataset.id = destination.id
 
-    showCard.append(exitBtn, name, img, dateVisited, hr1, address, category, visited, cost, attendees, hr2, comment, rating, hr3, editBtn, deleteBtn)
+    const editBtn = document.createElement("button")
+    editBtn.id = "edit-destination-btn"
+    editBtn.innerText = "Edit"
+    editBtn.dataset.id = destination.id
+    editBtn.addEventListener("click", fetchCurrentDest)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.id = "delete-destination-btn"
+    deleteBtn.innerText = "Delete"
+    deleteBtn.dataset.id = destination.id
+    deleteBtn.addEventListener("click", deleteDestination)
+
+    showCard.append(exitBtn, name, dateVisited, hr1, address, category, visited, cost, attendees, hr2, comment, rating, hr3, editBtn, deleteBtn)
     
     destinationList.hidden = true
     showCard.hidden = false
-
 }
 
-////////// EDIT SHOW CARD//////////////
-function editShowCard(e){
+////////// GET CURRENT INFO ON DEST THAT IS GETTING UPDATED //////////////
+function fetchCurrentDest(e) {
     const id = e.target.dataset.id
+    showCard.hidden = true
+    editDestDiv.hidden = false
 
+    fetch(`http://localhost:3000/destinations/${id}`)
+    .then(response => response.json())
+    .then(dest => {
+        renderEditDestForm(dest)
+    })
+}
+
+////////// GRAB ALL FORM FIELDS FROM UPDATE DEST FORM//////////////
+let updatedName = document.querySelector("#updated-name")
+let updatedDate = document.querySelector("#updated-date")
+let updatedCategory = document.querySelector("#updated-category")
+let updatedComment = document.querySelector("#updated-comment")
+let updatedVisited = document.getElementsByName("updated-visited")
+let updatedCost = document.querySelector("#updated-cost")
+let updatedAttendees = document.querySelector("#updated-attendees")
+let updatedRating = document.querySelector("#updated-rating")
+
+////////// POPULATE EDIT DEST FORM WITH EXISTING VALUES //////////////
+function renderEditDestForm(currentDest){
+    updatedName.value = currentDest.name
+    updatedDate.value = currentDest.date_visited
+    updatedCategory.value = currentDest.category
+    updatedComment.value = currentDest.comment
+    updatedVisited.value = currentDest.visited
+    updatedCost.value = currentDest.cost
+    updatedAttendees.value = currentDest.attendees
+    updatedRating.value = currentDest.rating
+    editDestForm.dataset.id = currentDest.id
  
+    editDestForm.addEventListener("submit", patchUpdateDest)
+}
+
+////////// PATCH REQUEST TO UPDATE DEST INFORMATION IN BACKEND //////////////
+function patchUpdateDest(e){
+    e.preventDefault();
+
+    let destId = e.target.dataset.id
+    fetch(`http://localhost:3000/destinations/${destId}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify ({
+            name: updatedName.value,
+            date_visited: updatedDate.value,
+            category: updatedCategory.value,
+            comment: updatedComment.value,
+            visited: editDestForm.updatedVisited.value,
+            cost: updatedCost.value,
+            attendees: updatedAttendees.value,
+            rating: updatedRating.value
+        })
+
+    })
+    .then(response => response.json())
+    .then(dest => {
+        makeNewDestCard(dest)
+        editDestDiv.hidden = true
+        removeOutdatedLi(dest);
+        addNewDestination(dest)
+        addMarker(dest, map)
+    })
+}
+
+////////// CANCEL BTN OUT OF UPDATE DEST FORM  /////////////
+editDestCancelBtn.addEventListener("click", () => {
+    newDestinationForm.reset() 
+    showCard.hidden = false
+    editDestDiv.hidden = true
+ })
+
+function removeOutdatedLi(dest){
+    let allNodeLis = destinationList.querySelectorAll("li")
+    let allLis = Array.from(allNodeLis)
+    let destinationId = dest.id
+    let wantedLi = allLis.find(item =>
+        item.dataset.destId === destinationId.toString()
+    )
+    wantedLi.remove();
 }
 
 
-////////// EXIT OUT OF SHW CARD/////////////
+////////// EXIT OUT OF CURRENT CARD CARD/////////////
 function exitOut(e){
-    console.log(e.target)
     destinationList.hidden = false
     showCard.hidden = true
-    showCard.innerHTML = ""
+    accountInfoCard.hidden = true
+    editAccountCard.hidden = true
+}
+
+
+////////// NAV BTN THAT DISPLAYS ACCOUNT INFO /////////////
+accountNavBtn.addEventListener("click", () => {
+    renderAccountInfo(currentUser);
+})
+////////// RENDER ACCOUNT INFO CARD //////////////
+function renderAccountInfo(user) {
+    accountInfoCard.hidden = false
+    destinationList.hidden = true
+    destinationFormDiv.hidden = true
+    showCard.hidden = true
+    editAccountCard.hidden = true
+
+    firstName.textContent = user.first_name
+    lastName.textContent = user.last_name
+    email.textContent = user.email
+
+    let exitBtn = document.querySelector("#account-exit")
+    exitBtn.addEventListener("click", exitOut)
+
+    let userEditBtn = document.querySelector(".account-info-card #edit-account-btn")
+    userEditBtn.addEventListener("click", renderEditAccountForm)
+    
+}
+
+////////// RENDER EDIT ACCOUNT FORM AND POPULATE WITH EXISTING DATA//////////////
+function renderEditAccountForm(){
+    accountInfoCard.hidden = true
+    editAccountCard.hidden = false
+    editAccountForm.hidden = false
+
+    let firstNameField = document.querySelector("#edit-account-form #new-first-name")
+    firstNameField.value = currentUser.first_name
+    let lastNameField = document.querySelector("#edit-account-form #new-last-name")
+    lastNameField.value = currentUser.last_name
+    let emailField = document.querySelector("#edit-account-form #new-email")
+    emailField.value = currentUser.email
+
+    let exitBtn = document.querySelector("#edit-account-exit")
+    exitBtn.addEventListener("click", exitOut)
+}
+
+////////// UPDATE ACCOUNT INFORMATION  /////////////
+editAccountForm.addEventListener("submit", updateAccount)
+function updateAccount(e){
+    e.preventDefault();
+    let userId = currentUser.id
+    let newFirstName = document.querySelector("#edit-account-form #new-first-name").value
+    let newLastName = document.querySelector("#edit-account-form #new-last-name").value
+    let newEmail = document.querySelector("#edit-account-form #new-email").value
+    
+    fetch(`http://localhost:3000/users/${userId}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            first_name: newFirstName,
+            last_name: newLastName,
+            email: newEmail
+        })
+    })
+    .then(response => response.json())
+    .then(newUserInfo => {
+        renderAccountInfo(newUserInfo)
+        currentUser = newUserInfo
+    })
+}
+
+///////// Delete Destination //////////
+function deleteDestination(e){
+    let id = e.target.dataset.id
+    fetch(`http://localhost:3000/destinations/${id}`, {
+        method: "DELETE"
+    })
+    .then(resp => resp.json())
+    .then(dest => {
+        removeOutdatedLi(dest);
+        fetchAllDestinations();
+        setTimeout(function(){renderMapMarker(map)}, 10)
+    })
+    showCard.hidden = true
+    destinationList.hidden = false  
 }
